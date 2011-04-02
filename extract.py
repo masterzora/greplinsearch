bca9e239e7c380f8d8cd956ab0790efd9593313b
Chris Sauro <masterzora@gmail.com>
2011-04-02 15:36:14 -0700

    Initial scaffolding

---
+++ extract.py
@@ -0,0 +1,7 @@
+def extract(document):
+    '''
+        input: document, the string to be indexed
+        ouput: a list of dicts, each indicating the start and end point
+                   and type of a found entity
+    '''
+    return []
