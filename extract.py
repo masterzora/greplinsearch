e90bccd9a08a7bfe0f754fc0792ffcc6f8d9b3ff
Chris Sauro <masterzora@gmail.com>
2011-04-02 16:57:41 -0700

    Added support for addresses as expected by the USPS

--- extract.py
+++ extract.py
@@ -1,9 +1,17 @@
 import re
 
-phonenumberregex = '(?:1[.\- ]?)?' # country code
-phonenumberregex += '\(?[\dA-Z]{3}\)?[.\- ]?' # area code
-phonenumberregex += '[\dA-Z]{3}[.\- ]?[\dA-Z]{4}' # and the rest
+phonenumberregex = '(?:\b\+?1[.\- ])?' # country code
+phonenumberregex += '\(?[\dA-Z]{3}\)?[.\- ]' # area code
+phonenumberregex += '[\dA-Z]{3}[.\- ][\dA-Z]{4}\W' # and the rest
+phonenumberregex += '|\+[\dA-Z]{11}' # 11-digits no separator means you have to use +
 phonenumber = re.compile(phonenumberregex)
+
+mailingaddressregex = '\d+' # Street number
+mailingaddressregex += '\s+[\dA-Za-z \-.\']+\s*\n\s*' # Street, Apt, etc
+mailingaddressregex += '[A-Za-z \-.\']+' # City
+mailingaddressregex += ',\s+[A-Z]{2}' # State
+mailingaddressregex += '\s+\d{5}(?:-\d{4})?' # ZIP code
+mailingaddress = re.compile(mailingaddressregex)
 
 def extract(document):
     '''
@@ -14,6 +22,8 @@ def extract(document):
     entities = []
     for m in phonenumber.finditer(document):
         entities += [{'start': m.start(), 'end': m.end(), 'type': 'phonenumber'}]
+    for m in mailingaddress.finditer(document):
+        entities += [{'start': m.start(), 'end': m.end(), 'type': 'address'}]
     return entities
 
 def extractfile(filename):
