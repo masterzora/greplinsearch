3a58d9a0b1f488c5f763ba023bd57c53a4c7f822
Chris Sauro <masterzora@gmail.com>
2011-04-02 17:46:05 -0700

    Apparently raw strings help make regexes work the way I expected.

--- extractregexes.py
+++ extractregexes.py
@@ -2,20 +2,21 @@ import re
 
 # precompiling regexes will make it faster for repeated operations
 
-phonenumberregex = '(?:\\b\+?1[.\- ])?' # country code
-phonenumberregex += '\(?[\dA-Z]{3}\)?[.\- ]' # area code
-phonenumberregex += '[\dA-Z]{3}[.\- ][\dA-Z]{4}\\b' # and the rest
-phonenumberregex += '|\+[\dA-Z]{11}' # 11-digits no separator means you have to use +
+phonenumberregex  = r"\b((?:\+?1[.\- ])?" # country code
+phonenumberregex += r"\(?[\dA-Z]{3}\)?[.\- ]" # area code
+phonenumberregex += r"[\dA-Z]{3}[.\- ][\dA-Z]{4}" # and the rest
+phonenumberregex += r"|\+[\dA-Z]{11})\b" # 11-digits no separator means you have to use +
 phonenumber = re.compile(phonenumberregex)
 
-mailingaddressregex = '\d+' # Street number
-mailingaddressregex += '\s+[\dA-Za-z \-.\']+\s*\n\s*' # Street, Apt, etc
-mailingaddressregex += '[A-Za-z \-.\']+' # City
-mailingaddressregex += ',\s+[A-Z]{2}' # State
-mailingaddressregex += '\s+\d{5}(?:-\d{4})?' # ZIP code
+mailingaddressregex  = r"\d+" # Street number
+mailingaddressregex += r"\s+[\dA-Za-z \-.\']+\s*\n\s*" # Street, Apt, etc
+mailingaddressregex += r"[A-Za-z \-.\']+" # City
+mailingaddressregex += r",\s+[A-Z]{2}" # State
+mailingaddressregex += r"\s+\d{5}(?:-\d{4})?" # ZIP code
 mailingaddress = re.compile(mailingaddressregex)
 
-flightconfirmationregex = '\\b([A-Z\d]{6}' # Most airlines use 6 alphanumerics
-flightconfirmationregex += '|\d{13}' # United
-flightconfirmationregex += ')\\b'
+flightconfirmationregex  = r"\b([A-Z\d]{6}" # Most airlines use 6 alphanumerics
+flightconfirmationregex += r"|\d{13}" # United
+flightconfirmationregex += r")\b"
 flightconfirmation = re.compile(flightconfirmationregex)
+
